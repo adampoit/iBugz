6fb0009c1c6e8ceab2f2a06bb2d149a8d2ce8f57
Adam Poit <metalkeys404@gmail.com>
2014-04-05 23:28:52 -0700

    Rename program to iBugz.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 import py2app
 
 setup(
-    name='FogBugz Tracker',
-    app=["FogBugzTracker.py"],
+    name='iBugz',
+    app=["iBugz.py"],
     data_files=["English.lproj"],
 )
